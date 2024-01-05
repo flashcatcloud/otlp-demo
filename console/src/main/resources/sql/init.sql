ab86d1ae1760f3955a79a27901f385d407495c7b
Jungzhang <zhanggen.Jung@gmail.com>
2024-01-05 16:05:37 +0800

    add: support logback for otel

--- console/src/main/resources/sql/init.sql
+++ console/src/main/resources/sql/init.sql
@@ -2,7 +2,6 @@ CREATE TABLE task
 (
     id             BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
     trace_id       VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'trace id',
-    parent_span_id VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'parent span id',
     task_name      VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'task name',
     task_status    VARCHAR(16) NOT NULL DEFAULT 'init' COMMENT 'task status'
 ) ENGINE = InnoDB
